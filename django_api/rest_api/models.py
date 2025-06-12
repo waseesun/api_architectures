fd3d96b0cb3669d86ae9932c05af57b70264548b
wasee-sun <revengersun@gmail.com>
2025-06-12 13:35:34 +0600

    Added Model Product

--- django_api/rest_api/models.py
+++ django_api/rest_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
