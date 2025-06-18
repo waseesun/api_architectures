8f6ef580c3fee2d07e4e01a9759d760ff6eebef9
wasee-sun <revengersun@gmail.com>
2025-06-18 10:19:00 +0600

    Removed payment app

--- django_api/payment_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Order
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['order_id', 'status', 'amount', 'payment_id', 'product_id', 'quantity', 'updated_at']
--- django_api/payment_app/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Order
-from .serializers import OrderSerializer
-
-class OrderViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    
